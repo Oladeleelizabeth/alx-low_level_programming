b8cda08df0bdec787fb635637c47f3b1564316ca
oladeleelizabeth <oladeleeniolaelizabeth@gmail.com>
2022-10-13 15:18:07 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a scrit that compiles a c file
+ *
+ * return zero
+ *
+*/
+ int main(void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+        return (0); 
+} 
