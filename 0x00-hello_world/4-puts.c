373fc555b320b46517485336ef62ae6bf8ed5112
oladeleelizabeth <oladeleeniolaelizabeth@gmail.com>
2022-10-13 20:16:14 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - entry point
  *
- *  Return: zero
+ * Return: zero
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
