65f2302f41d39476c7864e46a6411e786f55dda7
oladeleelizabeth <oladeleeniolaelizabeth@gmail.com>
2022-10-13 18:41:35 -0700

    hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - a scrit that compiles a c file
+ * main - entry point
  *
  * return: zero
- *
-*/
+ */
  int main(void) 
-{
+ {
 	puts("\"Programming is like building a multilingual puzzle");
         return (0); 
 } 
