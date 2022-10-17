7a7e108427b7150c156f55026b5571976e60c27a
oladeleelizabeth <oladeleeniolaelizabeth@gmail.com>
2022-10-17 16:42:20 -0700

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,33 +3,19 @@
 #include <stdlib.h>
 
 /**
-
  * main - put out
-
  *
-
  * Return: zero
-
  */
-
 int main(void)
-
 {
-
 	int x;
 
-
-
 	for (x = '0'; x <= '9'; x++)
-
 	{
 
 		putchar(x);
-
 	}
-
 	putchar('\n');
-
 	return (0);
-
 }
