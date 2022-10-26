4048165adcd8c6c05514c9dd53cf6986da5757d0
oladeleelizabeth <oladeleeniolaelizabeth@gmail.com>
2022-10-25 17:14:03 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_rev - print a number in reverse
+ * @s: a pointer
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length != 0)
+	{
+		for (; length >= 0; length--)
+		{
+			if (s[length] != '\0')
+			{
+				_putchar(s[length]);
+			}
+
+		}
+	}
+	_putchar('\n');
+}
+
